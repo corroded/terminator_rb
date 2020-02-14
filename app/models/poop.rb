31e1f8f3d37f36d49334732f59bdd49580003b6a
Eumir Gaspar <eumirgaspar@gmail.com>
2020-02-14 14:09:09 +0800

    Working protoype: can delete files set to be expired.

--- app/models/poop.rb
+++ app/models/poop.rb
@@ -1,4 +1,4 @@
-# @expire_at 2022-01-01
+# @expire_at 2019-01-01
 # @deprecated Some notes
 class Poop
   def initialize; end
--- bin/cleaner.rb
+++ bin/cleaner.rb
@@ -2,25 +2,20 @@ require 'open3'
 require 'pry'
 require 'time'
 
-command = "git grep -irz '@expires \\w'"
+command = "git grep -irz '@expire_at \\w'"
 Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
   files_with_dates = stdout.read
 
   files_with_dates.split("\n").each do |file_with_date|
-    filename, date = file_with_date.split("# @expires")
+    filename, date = file_with_date.split("# @expire_at")
     end_of_life = Time.parse(date)
 
-    if Time.now < end_of_life
-      puts "Skipping #{filename} - #{end_of_life} has not passed"
-      next
-    end
+    # NOTE: Not really fussed with time zones since these will be
+    # checked in the PR anyway.
+    next if Time.now < end_of_life
 
-    puts "About to edit #{filename} because #{end_of_life} has passed"
+    puts "About to delete #{filename.strip!} because #{end_of_life} has passed"
+
+    FileUtils.rm filename
   end
-  #
-  # File.open(filename, 'r+') do |file|
-  #   while line = f1.gets
-  #     puts line
-  #   end
-  # end
 end
