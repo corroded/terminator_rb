0de0a7e2655c7f4eaae857cc03ae1fd52fac68f0
Eumir Gaspar <eumirgaspar@gmail.com>
2020-02-14 11:44:43 +0800

    Tweaked cleaner to show files to be cleaned

--- bin/cleaner.rb
+++ bin/cleaner.rb
@@ -7,7 +7,7 @@ Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
   files_with_dates = stdout.read
 
   files_with_dates.split("\n").each do |file_with_date|
-    filename, date = file_with_date.split("\u0000# @expires")
+    filename, date = file_with_date.split("# @expires")
     end_of_life = Time.parse(date)
 
     if Time.now < end_of_life
@@ -17,12 +17,10 @@ Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
 
     puts "About to edit #{filename} because #{end_of_life} has passed"
   end
-
+  #
   # File.open(filename, 'r+') do |file|
   #   while line = f1.gets
   #     puts line
   #   end
   # end
-  #
-  # file.close
 end
