fee05991e3ed380177003ed837367455b8d8ec14
Eumir Gaspar <eumirgaspar@gmail.com>
2020-02-14 10:41:29 +0800

    Added script that prints out which files to delete

---
+++ bin/cleaner.rb
@@ -0,0 +1,28 @@
+require 'open3'
+require 'pry'
+require 'time'
+
+command = "git grep -irz '@expires \\w'"
+Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+  files_with_dates = stdout.read
+
+  files_with_dates.split("\n").each do |file_with_date|
+    filename, date = file_with_date.split("\u0000# @expires")
+    end_of_life = Time.parse(date)
+
+    if Time.now < end_of_life
+      puts "Skipping #{filename} - #{end_of_life} has not passed"
+      next
+    end
+
+    puts "About to edit #{filename} because #{end_of_life} has passed"
+  end
+
+  # File.open(filename, 'r+') do |file|
+  #   while line = f1.gets
+  #     puts line
+  #   end
+  # end
+  #
+  # file.close
+end
