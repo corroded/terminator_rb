b51ae13f9dbfb209dc9bba07ffd8a3081eeb5f63
Eumir Gaspar <eumirgaspar@gmail.com>
2020-03-20 16:49:04 +0800

    Updated bot to list down files to delete

--- glitch/app/index.js
+++ glitch/app/index.js
@@ -1,45 +1,55 @@
 const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({ auth: `5f4a7acd1d5ec5b5c753066363f75211105ada43` });
+const octokit = new Octokit({ auth: `token` });
 
-module.exports = (app) => {
+module.exports = async(app) => {
   // Your code here
   app.log('Yay! The app was loaded!')
-  
-  async function kek() {  
-    const result = await octokit.request("GET /repos/:owner/:repo/contents/FOR_TERMINATION", {
+
+  async function getFileContents(path) {
+    const result = await octokit.request(`GET /repos/:owner/:repo/contents/${path}`, {
       owner: "corroded",
       repo: "yolo_repo"
     });
-    
-    console.log(result.data)
-    
-    var b64string = result.data.content;
-    var buf = Buffer.from('YXBwL21vZGVscy9wb29wLnJiCnNwZWMvYWxzb19raWxsX3NwZWMucmIK\n', 'base64'); // Ta-da
-    
+
+    var b64String = result.data.content;
+    var buf = Buffer.from(b64String, 'base64'); // Ta-da
+
     console.log(buf.toString('utf-8'))
-    
-    var forTermination = buf.toString('utf-8')
-    console.log(forTermination.split("\n"))
-    forTermination.split("\n").forEach(function(x) {
-      if (x === '') {
+    return buf.toString('utf-8')
+  }
+
+  async function cleanup() {
+    var forTermination = await getFileContents('FOR_TERMINATION')
+
+    forTermination.split("\n").forEach(async function(fileToDelete) {
+      if (fileToDelete === '') {
         return
       }
-      
-      console.log("yolo" + x)
+
+      let fileContents = await getFileContents(fileToDelete)
+
+      console.log("-----")
+      console.log(fileToDelete)
+      let expiryDate = fileContents.match(/# @expire_at (?<expiryDate>.*)/).groups.expiryDate
+      if (expiryDate !== undefined) {
+        let expiry = new Date(Date.parse(expiryDate))
+
+        if (expiry < Date.now()) {
+          console.log(`About to delete ${fileToDelete}`)
+        }
+      } else {
+        console.log("nothing to delete")
+      }
     })
   }
-  
-  kek()
 
   // example of probot responding 'Hello World' to a new issue being opened
   app.on('push', async context => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    app.log(context)
-    const params = context.issue({body: 'Hello World!'})
 
-    app.log(params)
+    await cleanup()
     // Post a comment on the issue
     // return context.github.issues.createComment(params)
   })
